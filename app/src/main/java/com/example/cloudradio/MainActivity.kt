fc613321afec0c621bcdb3baea8b0081f2a9b318
leonardo204 <zerolive7@gmail.com>
2021-03-16 17:39:27 +0900

    최초 커밋 + 탭 기초 구현

---
+++ app/src/main/java/com/example/cloudradio/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.cloudradio
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "CloudRadio"
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
+
+        tabLayout.addTab( tabLayout.newTab().setText("OnAir"))
+        tabLayout.addTab( tabLayout.newTab().setText("Program"))
+        tabLayout.addTab( tabLayout.newTab().setText("More"))
+
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = MyAdapter(this, supportFragmentManager, tabLayout.tabCount)
+        viewPager.adapter = adapter
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+    }
+}
